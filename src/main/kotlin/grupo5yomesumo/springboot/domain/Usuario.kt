b725bb1e86e8f5b3c7e33ecf3afbb5d06d5af64c
lugant <lugant.development@gmail.com>
2024-04-17 15:11:36 -0300

    feat usuario borro dni

--- src/main/kotlin/grupo5yomesumo/springboot/domain/Usuario.kt
+++ src/main/kotlin/grupo5yomesumo/springboot/domain/Usuario.kt
@@ -3,10 +3,11 @@ package grupo5yomesumo.springboot.domain
 import grupo5yomesumo.springboot.repository.Entidad
 
 class Usuario(
-    override var id: Long,
+    override var id: Long = 0,
     val nombre: String,
     val apellido: String,
-    val dni: Int
+    val username: String,
+    val password: String,
     ): Entidad {
     /* Por ahora parece que no tiene alguna responsabilidad */
 }

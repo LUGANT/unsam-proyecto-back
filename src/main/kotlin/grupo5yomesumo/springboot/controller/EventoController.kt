c327499b63970d828d3c6bf718a2d30b9a0137ac
lugant <lugant.development@gmail.com>
2024-04-17 16:20:33 -0300

    feat endopoint agregar rechazar aceptar solicitudes

--- src/main/kotlin/grupo5yomesumo/springboot/controller/EventoController.kt
+++ src/main/kotlin/grupo5yomesumo/springboot/controller/EventoController.kt
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import grupo5yomesumo.springboot.service.EventoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 
 
 @RestController()
@@ -19,5 +22,20 @@ class EventoController(
     @GetMapping("all")
     @Operation(summary = "Devuelve todos los eventos")
     fun getAllEventos(): List<EventoDTO> = eventoService.getAllEventos().map{ EventoDTO(it) }
+
+    @PutMapping("{eventoId}/solicitud/aceptar/{usuarioId}")
+    @Operation(summary = "acepta la solicitud de un usuario")
+    fun aceptarSolicitud(@PathVariable eventoId: Long, @PathVariable usuarioId: Long) = eventoService.aceptarSolicitud(eventoId, usuarioId)
+
+    @PutMapping("{eventoId}/solicitud/enviar/{usuarioId}")
+    @Operation(summary = "envia solicitud a un usuario")
+    fun enviarSolicitud(@PathVariable eventoId: Long, @PathVariable usuarioId: Long) = eventoService.enviarSolicitud(eventoId, usuarioId)
+
+    @PutMapping("{eventoId}/solicitud/rechazar/{usuarioId}")
+    @Operation(summary = "rechazar solicitud a un usuario")
+    fun rechazarSolicitud(@PathVariable eventoId: Long, @PathVariable usuarioId: Long) = eventoService.rechazarSolicitud(eventoId, usuarioId)
+
+    @GetMapping("{eventoId}")
+    fun getEvento(@PathVariable eventoId: Long) = eventoService.getEvento(eventoId)
 
 }
--- src/main/kotlin/grupo5yomesumo/springboot/service/EventoService.kt
+++ src/main/kotlin/grupo5yomesumo/springboot/service/EventoService.kt
@@ -3,12 +3,35 @@ package grupo5yomesumo.springboot.service
 import grupo5yomesumo.springboot.domain.Evento
 import org.springframework.stereotype.Service
 import grupo5yomesumo.springboot.repository.EventoRepository
+import grupo5yomesumo.springboot.repository.UsuarioRepository
 
 @Service
 class EventoService (
-    val eventoRepository: EventoRepository
+    val eventoRepository: EventoRepository,
+    val usuarioRepository: UsuarioRepository
 ) {
 
     fun getAllEventos(): List<Evento> = eventoRepository.findAll()
+    fun getEvento(eventoId: Long): Evento = eventoRepository.getById(eventoId)
+    fun aceptarSolicitud(eventoId: Long, usuarioId: Long) {
+        val evento = eventoRepository.getById(eventoId)
+        val invitado = usuarioRepository.getById(eventoId)
+
+        evento.responderSolicitud(invitado, true)
+    }
+
+    fun rechazarSolicitud(eventoId: Long, usuarioId: Long) {
+        val evento = eventoRepository.getById(eventoId)
+        val invitado = usuarioRepository.getById(eventoId)
+
+        evento.responderSolicitud(invitado, false)
+    }
+
+    fun enviarSolicitud(eventoId: Long, usuarioId: Long) {
+        val evento = eventoRepository.getById(eventoId)
+        val invitado = usuarioRepository.getById(eventoId)
+
+        evento.agregarSolicitud(invitado)
+    }
 
 }

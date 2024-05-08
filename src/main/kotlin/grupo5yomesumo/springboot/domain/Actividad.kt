a55a2523ed4349e6799756f34e70280faf6fa7c3
Lucas Antenni <110508314+LUGANT@users.noreply.github.com>
2024-05-08 15:43:05 -0300

    Update Actividad.kt se agrego @Column en atributos

--- src/main/kotlin/grupo5yomesumo/springboot/domain/Actividad.kt
+++ src/main/kotlin/grupo5yomesumo/springboot/domain/Actividad.kt
@@ -6,7 +6,7 @@ import jakarta.persistence.*
 @Entity
 data class Actividad(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id : Long = 0,
-    val nombre : String = "",
-    val esGrupal : Boolean = false
+    @Column(length = 50) val nombre : String = "",
+    @Column val esGrupal : Boolean = false
 )
 

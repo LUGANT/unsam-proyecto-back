f018e5a2ffc7bd928194e75c51cd5eea5646a21b
Nico Masuyama <nicolasmasuyama@gmail.com>
2024-05-17 17:38:09 -0300

    cambio id de mensaje

--- src/main/kotlin/grupo5yomesumo/springboot/domain/Chat.kt
+++ src/main/kotlin/grupo5yomesumo/springboot/domain/Chat.kt
@@ -3,13 +3,13 @@ package grupo5yomesumo.springboot.domain
 import java.time.LocalDateTime
 
 data class Chat(
-    val id : String = "",
+    val id : String,
     val evento : Long,
     val Mensajes : List<Mensaje>
 )
 
 data class Mensaje(
-    val id: Long,
+    val id: String,
     val usuario : Long,
     val horario : LocalDateTime,
     val texto : String

a4c404da8ac955b7a42ecb74c98f947f7b82d64e
lugant <lugant.development@gmail.com>
2024-05-24 14:49:45 -0300

    implementación mongo docker y dominio

--- src/main/kotlin/grupo5yomesumo/springboot/domain/Chat.kt
+++ src/main/kotlin/grupo5yomesumo/springboot/domain/Chat.kt
@@ -1,5 +1,8 @@
 package grupo5yomesumo.springboot.domain
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
 import java.time.LocalDateTime
 
 //data class Chat(
@@ -8,10 +11,11 @@ import java.time.LocalDateTime
 //    val Mensajes : List<Mensaje>
 //)
 
+@Document
 data class Mensaje(
-    val id: String,
-    val evento: Long,
-    val usuario : Long,
+    @Id val id: String,
+    @Field("eventoId") val evento: Long,
+    @Field("usuarioId") val usuario : Long,
     val horario : LocalDateTime,
     val texto : String
 )

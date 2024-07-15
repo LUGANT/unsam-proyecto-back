dcca0b007509001c67bdd3a0a95aaa747d4eb44d
rtartaglia00 <rtartaglia@estudiantes.unsam.edu.ar>
2024-07-15 20:03:35 -0300

    ultimos cambios chat

--- src/main/kotlin/grupo5yomesumo/springboot/config/SocketIOService.kt
+++ src/main/kotlin/grupo5yomesumo/springboot/config/SocketIOService.kt
@@ -12,15 +12,20 @@ import grupo5yomesumo.springboot.repository.ChatRepository
 import grupo5yomesumo.springboot.repository.MensajeRepository
 import jakarta.annotation.PostConstruct
 import jakarta.annotation.PreDestroy
+import org.bson.Document
 import org.springframework.stereotype.Service
 import java.util.concurrent.Executors
 
 
 @Service
 class SocketIOService (
-    var mensajeRepository : MensajeRepository,
     var chatRepository: ChatRepository
 ) {
+
+    private val mongoClient = MongoClients.create("mongodb://mongo:mongo@localhost:27021")
+    private val database: MongoDatabase = mongoClient.getDatabase("yomesumo-mensajes")
+    private val collection: MongoCollection<Document> = database.getCollection("chat")
+    private val executor = Executors.newSingleThreadExecutor()
 
     private val config = Configuration().apply {
         hostname = "localhost"
@@ -30,23 +35,19 @@ class SocketIOService (
 
 
     private val server = SocketIOServer(config)
-    private val mongoClient = MongoClients.create("mongodb://localhost:27021")
-    private val database: MongoDatabase = mongoClient.getDatabase("yomesumo-mensajes")
-    private val collection: MongoCollection<org.bson.Document> = database.getCollection("mensaje")
-    private val executor = Executors.newSingleThreadExecutor()
+
 
     @PostConstruct
     fun startServer() {
+
+        server.addEventListener("eventoId", Long::class.java) { client, data, ackRequest ->
+            val mensajesAnteriores = chatRepository.findByEventoId(data)?.mensajes //el eventoId tendria que ser dinamico
+            if(mensajesAnteriores != null) {
+                client.sendEvent("initial messages", mensajesAnteriores)
+            }
+        }
+
         server.addEventListener("chat message", ChatMensajeDTO::class.java) { client, data, ackRequest ->
-
-            //Busca el chat existente, sino crea uno
-            val chat = chatRepository.findByEventoIdOrUserIds(data.eventoId, data.userIds) ?: Chat(
-                eventoId = data.eventoId,
-                userIds = data.userIds.toList(),
-                mensajes = mutableListOf()
-            )
-
-//
 
             // Crea el mensaje
             val mensaje = Mensaje(
@@ -55,18 +56,28 @@ class SocketIOService (
                 texto = data.texto
             )
 
-            // Agrega el mensaje al chat
-            chat.mensajes.add(mensaje)
+            val chat = chatRepository.findByEventoId(data.eventoId)
+            if (chat != null) {
+                // Si existe el chat, agrega el mensaje a la lista
+                chat.mensajes.add(mensaje)
+                chatRepository.save(chat)
+                println("Mensaje Recibido: ${chat.mensajes.last().texto} a las --")
 
-            // Guarda el chat
-            chatRepository.save(chat)
+            } else {
+                // Si no existe el chat, crea uno nuevo
+                val newChat = Chat(
+                    eventoId = data.eventoId,
+                    userIds = data.userIds,
+                    mensajes = mutableListOf(mensaje)
+                )
+                chatRepository.save(newChat)
+            }
 
-            println("Mensaje Recibido: ${chat.mensajes.last().texto} at --")
 
             broadcastMessage("chat message", data)
 
-            // Start Change Stream listener
-            executor.submit { listenForChanges() }
+//            // Start Change Stream listener
+//            listenForChanges()
         }
 
         server.start()
@@ -75,28 +86,45 @@ class SocketIOService (
 
     private fun listenForChanges() {
         val changeStream = database.getCollection("chat").watch().iterator()
+        println("Listening for changes...")
         while (changeStream.hasNext()) {
             val change = changeStream.next()
+            println("Change detected: $change")
             if (change.operationType == OperationType.INSERT || change.operationType == OperationType.UPDATE) {
                 val fullDocument = change.fullDocument ?: continue
-                val mensajesDocument = fullDocument.getList("mensajes", org.bson.Document::class.java)
-                val mensajes = mensajesDocument.map { mensajeDoc ->
-                    Mensaje(
-                        texto = mensajeDoc.getString("texto"),
-                        usuarioId = mensajeDoc.getLong("usuarioId")
-                        // Añadir cualquier otro campo necesario
+                val mensajesDocument = fullDocument.getList("mensajes", Document::class.java).map { doc ->
+                    ChatMensajeDTO(
+                        texto = doc.getString("texto"),
+                        usuarioId = doc.getLong("usuarioId"),
+                        eventoId = fullDocument.getLong("eventoId"),
+                        userIds = fullDocument.getList("userIds", Long::class.java)
                     )
                 }
-                val chatMensajeDTO = ChatMensajeDTO(
-                    texto = mensajes.last().texto,
-//                    horario = LocalDateTime.parse(fullDocument.getString("horario")),
-                    usuarioId = mensajes.last().usuarioId,
-                    eventoId = fullDocument.getLong("eventoId"),
-                    userIds = fullDocument.getList("userIds", Long::class.java)
-                )
-                server.allClients.forEach { client ->
-                    client.sendEvent("chat message", chatMensajeDTO)
+//                val mensajes = mensajesDocument.map { mensajeDoc ->
+//                    Mensaje(
+//                        texto = mensajeDoc.getString("texto"),
+//                        usuarioId = mensajeDoc.getLong("usuarioId")
+//                        // Añadir cualquier otro campo necesario
+//                    )
+//                }
+//                val chatMensajeDTO = ChatMensajeDTO(
+//                    texto = mensajes.last().texto,
+////                    horario = LocalDateTime.parse(fullDocument.getString("horario")),
+//                    usuarioId = mensajes.last().usuarioId,
+//                    eventoId = fullDocument.getLong("eventoId"),
+//                    userIds = fullDocument.getList("userIds", Long::class.java)
+//                )
+
+                mensajesDocument.forEach { message ->
+                    println("Sending change: $message")
+                    server.allClients.forEach { client ->
+                        client.sendEvent("chat message", message)
+                    }
                 }
+
+//                server.allClients.forEach { client ->
+//                    client.sendEvent("chat message", message)
+//                }
             }
         }
     }
